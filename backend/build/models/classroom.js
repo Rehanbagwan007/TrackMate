4ffd7d930df50a15570bc27431242ac8c2e3b678
Rehanbagwan007 <rehanbagwan10000@gmail.com>
2026-01-04 18:01:07 +0530

    updated docker for backend

--- backend/build/models/classroom.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// Placeholder file describing Classroom model usage for future attendance.
-// Actual DB model defined in Prisma schema; this helper centralizes future logic.
-Object.defineProperty(exports, "__esModule", { value: true });
-// Use this file later to add business logic for classroom management
